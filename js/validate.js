35e96afbb6d41ea5bb0144872433ea3354ca77e9
Carla Gatica <carla.gatica@gmail.com>
2023-05-13 12:02:35 -0300

    avalidacion funcionando modificado el header

--- js/validate.js
+++ js/validate.js
@@ -1,14 +1,16 @@
 var modal = document.getElementById("myModal"); 
 var btn = document.getElementById("myBtn"); 
 var span = document.getElementsByClassName("close")[0]; 
-btn.onclick = function() { 
+
+
+function validateForm() {
     modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
+    span.onclick = function() {
         modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
 }
