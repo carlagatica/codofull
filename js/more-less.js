400c9d0b89bc5820207a8c84724fbfb3588ccab2
Yair Ezequiel <120728969+Yair0912@users.noreply.github.com>
2023-05-24 13:07:22 -0300

    Update more-less.js

--- js/more-less.js
+++ js/more-less.js
@@ -13,3 +13,9 @@ function moreLess() {
       moreText.style.display = "inline";
     }
   }
+
+let more = document.querySelectorAll('.more');
+for (let i=0; i<more.length; i++)
+{
+  more[i].addEventListener('click',function(){more[i].parentNode.classList.toggle('active') })
+}
