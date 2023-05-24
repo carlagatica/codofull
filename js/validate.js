2a24b0d31447ee7ec6736a67e773c04a726e139e
paosarabia <mariapsarabia@gmail.com>
2023-05-24 14:12:44 -0300

    api para form adopciones y participa

--- js/validate.js
+++ js/validate.js
@@ -2,6 +2,8 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn"); 
 var span = document.getElementsByClassName("close")[0];
 var contactForm = document.getElementById("contactForm");
+var partipateForm = document.getElementById("participateForm");
+var adoptionsForm = document.getElementById("adoptionsForm");
 
 var myHeaders = new Headers();
 myHeaders.append("apikey", "jQW6wRgyo7BQ6q7pNhXldBMWTH7nGAnz");
@@ -22,6 +24,68 @@ contactForm.addEventListener("submit", async function(event){
     event.preventDefault();
 try {
     const response = await fetch(`https://api.apilayer.com/email_verification/${emailValue}`, requestOptions);
+    if (!response.ok) {
+        throw new Error("API request failed");
+    }
+    const result = await response.text();
+    console.log(result);
+    alert("EL EMAIL INGRESADO ES VALIDO");
+} catch (error) {
+    alert("Error en la solicitud del API o EMAIL INGRESADO NO VALIDO");
+}
+
+    modal.style.display = "block";
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
+partipateForm.addEventListener("submit", async function(event){
+
+    var emailpInput = document.getElementById("emailFormParticipate");
+    var emailpValue = emailpInput.value;
+    console.log(emailpValue);
+
+    //*SE PREVIENE EL ENVIO DE DATOS DEL FORMULARIO A UN SERVIDOR, YA QUE AUN NO EXISTE *//
+    event.preventDefault();
+try {
+    const response = await fetch(`https://api.apilayer.com/email_verification/${emailpValue}`, requestOptions);
+    if (!response.ok) {
+        throw new Error("API request failed");
+    }
+    const result = await response.text();
+    console.log(result);
+    alert("EL EMAIL INGRESADO ES VALIDO");
+} catch (error) {
+    alert("Error en la solicitud del API o EMAIL INGRESADO NO VALIDO");
+}
+
+    modal.style.display = "block";
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
+adoptionsForm.addEventListener("submit", async function(event){
+
+    var emailaInput = document.getElementById("emailFormAdoptions");
+    var emailaValue = emailaInput.value;
+    console.log(emailaValue);
+
+    //*SE PREVIENE EL ENVIO DE DATOS DEL FORMULARIO A UN SERVIDOR, YA QUE AUN NO EXISTE *//
+    event.preventDefault();
+try {
+    const response = await fetch(`https://api.apilayer.com/email_verification/${emailaValue}`, requestOptions);
     if (!response.ok) {
         throw new Error("API request failed");
     }
